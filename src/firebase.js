d9b34dc9388c3bf833f67a1067f224d313ed12ad
Vishal <vishalpal1501@gmail.com>
2023-05-23 17:03:27 +0530

    Configure the firebase inside the src folder file name is firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDrs3lj8_KYn2hIpO2yocbbdMhC2EvA-0s',
+  authDomain: 'react-hook-blog-6d3ac.firebaseapp.com',
+  projectId: 'react-hook-blog-6d3ac',
+  storageBucket: 'react-hook-blog-6d3ac.appspot.com',
+  messagingSenderId: '384337911751',
+  appId: '1:384337911751:web:49e31832003c1e5fa683e2',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
