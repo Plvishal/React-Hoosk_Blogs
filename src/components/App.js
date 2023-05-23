3d671e62f27de12a2325a386812b5912509cb29f
Vishal <vishalpal1501@gmail.com>
2023-05-23 17:54:30 +0530

    Setting the route for blogs and created some file a like Home.js CreatePost.js Navbar.js PostDetail.js index.js inside the components folder which is inside the src folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Navbar } from './Navbar';
+import { CreatePost } from './CreatePost';
+import { Home } from './Home';
+import { PostDetail } from './PostDetail';
 
 function App() {
   return (
-    <div className="App">
-      <p>....................................................</p>
+    <div className="container">
+      <Navbar />
+      <Routes>
+        <Route exact path="/" Component={Home} />
+        <Route exact path="/past/:postId" Component={PostDetail} />
+        <Route exact path="/create-post" Component={CreatePost} />
+      </Routes>
     </div>
   );
 }
